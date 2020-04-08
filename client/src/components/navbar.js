87f45000497e970daa0241390bd227c208d3a3b1
Bryan Jerger <Bjerger1470@gmail.com>
2020-04-07 22:47:09 -0500

    app.css in assets

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { Route, Link } from 'react-router-dom'
-import '../App.css';
+import '../components/assets/css/App.css';
 import axios from 'axios'
 
 class Navbar extends Component {
